f360da5dc0757ce3bb42cfb86b72740fc0253566
Uğur YILDIZ <uggurryldz@gmail.com>
2025-02-06 19:56:24 +0300

    Doğrulama Kodu Gönderme işlemi tamamlandı

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -64,6 +64,8 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
           client.Send(mimeMessage);
           client.Disconnect(true);
 
+          TempData["Mail"] = appUserRegisterDto.Email;
+
           return RedirectToAction("Index","ConfirmMail");
         }
         else
